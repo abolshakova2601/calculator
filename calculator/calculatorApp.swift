9494b9bad54dc416b3bb1fb9040e21cd2557db2b
Anna <ab@bk.ru>
2021-09-15 13:52:16 +0300

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by user on 15.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
